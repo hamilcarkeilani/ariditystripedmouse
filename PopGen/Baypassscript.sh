b0fd60d9120c5247bc4b7c6cbe9c5e148a928b30
hamilcarkeilani <hamilcarkeilani98@gmail.com>
2025-07-25 17:13:59 +0200

    Create Baypassscript.sh

---
+++ PopGen/Baypassscript.sh
@@ -0,0 +1,4 @@
+#Running BayPass Version 2.4 with the following command lines (3 different runs with 3 different seeds) :
+g_baypass -gfile carole.smadja/Téléchargements/Baypassinputfinal -efile efile -outprefix FinalRb_V1 -seed 5002
+g_baypass -gfile carole.smadja/Téléchargements/Baypassinputfinal -efile efile -outprefix FinalRb_V1 -seed 5003
+g_baypass -gfile carole.smadja/Téléchargements/Baypassinputfinal -efile efile -outprefix FinalRb_V1 -seed 5004
