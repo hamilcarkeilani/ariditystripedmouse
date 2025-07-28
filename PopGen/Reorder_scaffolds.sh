876c19cf98fa23bc08a197d73f5a30afd9fc2900
hamilcarkeilani <hamilcarkeilani98@gmail.com>
2025-07-28 21:44:45 +0200

    Update Reorder_scaffolds.sh

--- PopGen/Reorder_scaffolds.sh
+++ PopGen/Reorder_scaffolds.sh
@@ -1,1 +1,2 @@
-bcftools view -T contigs.bed 41-Passerculus_final.vcf.gz -Oz -o 1MB_bcftools_filtered_output.vcf.gz
+#Script to reorder scaffolds 
+bcftools view -T scaffold_list vcf.vcf.gz -Oz -o new_vcf.vcf.gz
