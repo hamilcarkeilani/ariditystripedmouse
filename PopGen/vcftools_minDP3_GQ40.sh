0695254f13870fe101ce35e7564fb3041e2abd8c
hamilcarkeilani <hamilcarkeilani98@gmail.com>
2025-07-29 00:03:52 +0200

    Update vcftools_minDP3_GQ40.sh

--- PopGen/vcftools_minDP3_GQ40.sh
+++ PopGen/vcftools_minDP3_GQ40.sh
@@ -1,1 +1,2 @@
+#Filtering script to remove all sites with a genotype quality under 40 and depth under 3
 vcftools --vcf populations.snps.vcf --minDP 3 --minGQ 40 --out populations.snps_minDP3_minGQ40_Rb --recode
