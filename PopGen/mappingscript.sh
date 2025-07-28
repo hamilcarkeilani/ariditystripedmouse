a910e117efcf19091bafbf5ee62f4a6d367048b2
hamilcarkeilani <hamilcarkeilani98@gmail.com>
2025-07-28 23:46:25 +0200

    Update mappingscript.sh

--- PopGen/mappingscript.sh
+++ PopGen/mappingscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# script ÃƒÂ  mettre dans le rÃƒÂ©pertoire juste avant les rÃƒÂ©pertoires avec les fastq et le rÃƒÂ©pertoire d'output
+# script to put in the directory right before the one containing the fastq files and the output directory
 
 SAMPLELIST=list_sample #path to the list of PREFIX for each fastq file (basically without the 1.fq or 2.fq)
 OUTPUTDIR=/media/bigvol/afiston/Work/4-project_PopGenMouse/5-Demultiplexage/NovaSEQ_run1/FASTQ_DEMULTI_V2/ #path to where the demultiplexed fastq are
@@ -19,8 +19,7 @@ FMDIR=/media/bigvol/afiston/Work/4-project_PopGenMouse/5-Demultiplexage/NovaSEQ_
 FIXMATESUFFIX=_fixmate.bam
 
 for SAMPLEFILE in `cat $SAMPLELIST`; do
-#ne pas oublier d'indexer le gÃ©nome de rÃ©fÃ©rence avant avec bowtie2-build ou bwa index selon le programme utilisÃ©
-# Ã  choisir un des deux programmes pour le mapping : bwa ou bowtie2, enlever la ligne Ã  ne pas utiliser
+#don't forget to index the genome before this step with bowtie-build for example
  sudo bowtie2 -x $REFINDEX -1 $OUTPUTDIR$SAMPLEFILE$SUFFIXDEMULTI1 -2 $OUTPUTDIR$SAMPLEFILE$SUFFIXDEMULTI2 -S $SAMDIR$SAMPLEFILE$SAMSUFFIX --reorder -p 4
         samtools view -b -o $BAMDIR$SAMPLEFILE$BAMSUFFIX $SAMDIR$SAMPLEFILE$SAMSUFFIX
         samtools sort -n -o $NSDIR$SAMPLEFILE$NAMESORTSUFFIX $BAMDIR$SAMPLEFILE$BAMSUFFIX
