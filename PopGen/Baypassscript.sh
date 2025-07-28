22df8b3c3ad676322f43bc91a53962740bd65c5b
hamilcarkeilani <hamilcarkeilani98@gmail.com>
2025-07-28 15:45:16 +0200

    Update Baypassscript.sh

--- PopGen/Baypassscript.sh
+++ PopGen/Baypassscript.sh
@@ -1,4 +1,4 @@
-#Running BayPass Version 2.4 with the following command lines (3 different runs with 3 different seeds) :
-g_baypass -gfile carole.smadja/Téléchargements/Baypassinputfinal -efile efile -outprefix FinalRb_V1 -seed 5002
-g_baypass -gfile carole.smadja/Téléchargements/Baypassinputfinal -efile efile -outprefix FinalRb_V1 -seed 5003
-g_baypass -gfile carole.smadja/Téléchargements/Baypassinputfinal -efile efile -outprefix FinalRb_V1 -seed 5004
+#Running BayPass Version 2.4 (in the directory where baypass is stored) with the following command lines (3 different runs with 3 different seeds) 
+g_baypass -gfile carole.smadja/Téléchargements/Baypassinputfinal -efile efile -outprefix Rb_V1 -seed 5002 &;
+g_baypass -gfile carole.smadja/Téléchargements/Baypassinputfinal -efile efile -outprefix Rb_V2 -seed 5003 &;
+g_baypass -gfile carole.smadja/Téléchargements/Baypassinputfinal -efile efile -outprefix Rb_V3 -seed 5004 &
